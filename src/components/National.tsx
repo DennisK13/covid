d60f97b974bdae3722fbcae0c442c1b9fe84ad60
DennisK13 <dennis.kroff@gmail.com>
2022-01-20 22:32:00 -0500

    commit on master, updated button ui

--- src/components/National.tsx
+++ src/components/National.tsx
@@ -9,47 +9,28 @@ import {
   Legend,
   ResponsiveContainer,
   Line,
-  Area,
-  AreaChart,
 } from "recharts";
-// function that removes commas from numbers
-const removeCommas = (num: any) => {
-  return num.toString().replace(/,/g, "");
-};
-function movingAverage(arr: any, n: number) {
-  let movingAverage = [];
-  let movingSum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (i < n) {
-      movingSum += arr[i];
-      movingAverage.push((movingSum / (i + 1)).toFixed(2));
-    } else {
-      movingSum += arr[i];
-      movingSum -= arr[i - n];
-      movingAverage.push((movingSum / n).toFixed(2));
-    }
-  }
-  return movingAverage;
-}
-export const National = ({ data, timeseries }: any) => {
+
+export const National = ({ data, timeseries, aspect }: any) => {
   const [timeline, setTimeline] = useState(90);
   const [nationalChart, setNationalChart] = useState<any>([]);
 
   let canada = data.filter((item: any) => item.name === "Canada");
-  useEffect(() => {
+  let chart = timeseries.filter((item: any) => item.name === "Canada");
+  let moving_deaths = movingAverage(
+    chart.map((x: any) => x.deaths),
+    7
+  );
 
-    let chart = timeseries.filter((item: any) => item.name === "Canada");
-    let moving_deaths = movingAverage(
-      chart.map((x: any) => x.deaths),
-      7
-    );
-  
-    chart = chart.map((x: any, i: number) => ({
-      ...x,
-      moving_deaths: parseInt(moving_deaths[i]),
-    }));
+  chart = chart.map((x: any, i: number) => ({
+    ...x,
+    moving_deaths: parseInt(moving_deaths[i]),
+  }));
+
+  useEffect(() => {
     setNationalChart(chart.slice(-timeline));
   }, [timeline]);
+
   return (
     <div style={{ border: "2px black solid" }}>
       <div>
@@ -79,68 +60,122 @@ export const National = ({ data, timeseries }: any) => {
           </div>
         ))}
       </div>
-      <div>
-        <div className="center row">
-          <div style={{ width: "45%" }}>
-            <div className="row" style={{ margin: "20px" }}>
-              <ResponsiveContainer width="100%" aspect={1}>
-                <LineChart width={700} height={500} data={nationalChart}>
-                  <CartesianGrid
-                    strokeDasharray="3 3"
-                    horizontal={true}
-                    vertical={false}
-                  />
-                  <XAxis dataKey="date" />
-                  <YAxis />
-                  <Tooltip />
-                  <Legend verticalAlign="top" />
+      <div className="chart center">
+        <div className="column">
+          <ResponsiveContainer width="100%" aspect={aspect}>
+            <LineChart data={nationalChart}>
+              <CartesianGrid
+                strokeDasharray="3 3"
+                horizontal={true}
+                vertical={false}
+              />
+              <XAxis dataKey="date" />
+              <YAxis width={50} />
+              <Tooltip />
+              <Legend verticalAlign="top" />
 
-                  <Line
-                    dataKey={"cases"}
-                    name="Cases"
-                    fill="#8884d8"
-                    dot={false}
-                    strokeWidth={2}
-                    type={"monotone"}
-                  />
-                </LineChart>
-              </ResponsiveContainer>
-            </div>
-          </div>
-          <div style={{ width: "45%" }}>
-            <div className="row" style={{ margin: "20px" }}>
-              <ResponsiveContainer width="100%" aspect={1}>
-                <LineChart width={700} height={500} data={nationalChart}>
-                  <CartesianGrid
-                    strokeDasharray="3 3"
-                    horizontal={true}
-                    vertical={false}
-                  />
-                  <XAxis dataKey="date" />
-                  <YAxis />
-                  <Tooltip />
-                  <Legend verticalAlign="top" />
-                  <Line
-                    dataKey={"moving_deaths"}
-                    name="Moving Average Deaths"
-                    stroke="darkred"
-                    fill="red"
-                    dot={false}
-                    strokeWidth={2}
-                    type={"monotone"}
-                  />
-                </LineChart>
-              </ResponsiveContainer>
-            </div>
-          </div>
+              <Line
+                dataKey={"cases"}
+                name="Cases"
+                fill="#8884d8"
+                dot={false}
+                strokeWidth={2}
+                type={"monotone"}
+              />
+            </LineChart>
+          </ResponsiveContainer>
         </div>
-        <div className="row center" style={{ padding: "25px" }}>
-          <button onClick={() => setTimeline(90)}>3 months</button>
-          <button onClick={() => setTimeline(180)}>6 months</button>
-          <button onClick={() => setTimeline(365)}>12 months</button>
-          <button onClick={() => setTimeline(0)}>All Time</button>
+
+        <div className="column">
+          <ResponsiveContainer width="100%" aspect={aspect}>
+            <LineChart width={700} data={nationalChart}>
+              <CartesianGrid
+                strokeDasharray="3 3"
+                horizontal={true}
+                vertical={false}
+              />
+              <XAxis dataKey="date" />
+              <YAxis width={50} />
+              <Tooltip />
+              <Legend verticalAlign="top" />
+              <Line
+                dataKey={"moving_deaths"}
+                name="Moving Average Deaths"
+                stroke="darkred"
+                fill="red"
+                dot={false}
+                strokeWidth={2}
+                type={"monotone"}
+              />
+            </LineChart>
+          </ResponsiveContainer>
         </div>
+      </div>
+
+      <div className="row center" style={{ padding: "25px" }}>
+        <button
+          onClick={() => {
+            setTimeline(90);
+          }}
+          style={{
+            backgroundColor: timeline === 90 ? "red" : "gray",
+            color: "white",
+            minWidth: "80px",
+            minHeight: "25px",
+          }}
+        >
+          3 months
+        </button>
+        <button
+          onClick={() => setTimeline(180)}
+          style={{
+            backgroundColor: timeline === 180 ? "red" : "gray",
+            color: "white",
+            minWidth: "80px",
+            minHeight: "25px",
+          }}
+        >
+          6 months
+        </button>
+        <button
+          onClick={() => setTimeline(365)}
+          style={{
+            backgroundColor: timeline === 365 ? "red" : "gray",
+            color: "white",
+            minWidth: "80px",
+            minHeight: "25px",
+          }}
+        >
+          12 months
+        </button>
+        <button
+          onClick={() => setTimeline(0)}
+          style={{
+            backgroundColor: timeline === 0 ? "red" : "gray",
+            color: "white",
+            minWidth: "80px",
+            minHeight: "25px",
+          }}
+        >
+          All Time
+        </button>
       </div>
     </div>
   );
 };
+
+function movingAverage(arr: any, n: number) {
+  let movingAverage = [];
+  let movingSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (i < n) {
+      movingSum += arr[i];
+      movingAverage.push((movingSum / (i + 1)).toFixed(2));
+    } else {
+      movingSum += arr[i];
+      movingSum -= arr[i - n];
+      movingAverage.push((movingSum / n).toFixed(2));
+    }
+  }
+  return movingAverage;
+}
